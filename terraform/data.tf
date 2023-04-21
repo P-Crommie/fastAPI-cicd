09826b1db2ef08cd9c9f59e2a940dbae9c79a771
Emmanuel Cromwell <emmanuelcromwell@ymail.com>
2023-04-21 10:32:59 +0000

    fixed errors in lambda files (terraform)

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,4 +1,4 @@
-data "aws_s3_bucket_object" "this" {
+data "aws_s3_object" "this" {
   bucket = "crommie-s3"
   key    = "api.zip"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,10 @@
-resource "aws_lambda_function" "my_lambda_function" {
+resource "aws_lambda_function" "this" {
   function_name    = "crommie-lambda-function"
+  s3_bucket        = "crommie-s3"
+  s3_key           = "api.zip"
   handler          = "main.handler"
   role             = aws_iam_role.lambda_exec.arn
   runtime          = "python3.7"
-  source_code_hash = data.aws_s3_bucket_object.this.etag
+  source_code_hash = data.aws_s3_object.this.etag
 }
 
